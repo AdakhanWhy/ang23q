c06b255898bb819134c45cd85986288fee2c28f6
AdakhanWhy <openpy@mail.ru>
2024-07-08 20:18:28 +0600

    Added youtube-client-app submodule

---
+++ src/app/search/search-response/search-response.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-response',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-response.component.html',
+  styleUrl: './search-response.component.scss'
+})
+export class SearchResponseComponent {
+
+}
